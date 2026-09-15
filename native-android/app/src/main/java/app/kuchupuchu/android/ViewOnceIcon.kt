a5059ceba0da7bcd20a1b5e897e097582ee091c9
rabbihossainltd1 <Rabbihossainltd@gmail.com>
2026-09-15 21:23:25 +0000

    r37-1fix: import nativeCanvas for the owner's-1 digit (apk compile)

--- native-android/app/src/main/java/app/kuchupuchu/android/ViewOnceIcon.kt
+++ native-android/app/src/main/java/app/kuchupuchu/android/ViewOnceIcon.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.Dp
 import kotlin.math.max
